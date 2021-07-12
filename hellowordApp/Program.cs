d430f96031e21cf1a2ac0a2e2a4b18f928ccbb35
NIsabbir <sabbirrahman682@gmail.com>
2021-07-12 12:17:44 +0600

    first commit

---
+++ hellowordApp/Program.cs
@@ -0,0 +1,18 @@
+﻿  using System;
+
+namespace hellowordApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(i);
+            }
+            
+              
+            Console.ReadKey();
+        }
+    }
+}
